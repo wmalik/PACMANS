38cf9fa24c48ef5446890a1af00466605a8d1f39
unknown <Administrator@.(none)>
2011-03-29 04:20:15 +0100

    Added additional server config files, some refactoring in the server code and implemented connectivity for consistency services

---
+++ Server/ServerAction.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Common.Util;
+using Common.Beans;
+using System.Xml;
+using System.Runtime.Remoting;
+using System.Runtime.Remoting.Channels.Tcp;
+using System.Runtime.Remoting.Channels;
+using System.Net.Sockets;
+using Common.Slots;
+using Common.Services;
+using PuppetMaster;
+using System.Collections;
+using Server.Services;
+
+namespace Server
+{
+    public class ServerAction : MarshalByRefObject, IConsistencyService
+    {
+
+        public bool WriteSequenceNumber(int seqNum)
+        {
+            Console.WriteLine("WriteSeqnum successfully invoked,{0}", seqNum);
+            return false;
+        }
+
+
+        public int ReadSequenceNumber()
+        {
+            return 0;
+        }
+
+    }
+}
